b1b78372b32d612548e6ba8a655ffb89c1342a71
mrinnnmoy <mrinmoy202000@gmail.com>
2023-03-31 11:23:34 +0530

    deploy-contract-truffle

--- web/contracts/demo.sol
+++ web/contracts/demo.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.19;
 contract demo{
     uint number;
     function set(uint _number) public {
-        number=_number;
+        number=_number + 1;
     }
     function get() public view returns(uint){
         return number;
